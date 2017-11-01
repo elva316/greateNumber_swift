57aa7237439b84d604c661f6a9a6ae409a6cdcd5
Elva Wang <elva316@gmail.com>
2017-10-31 22:58:56 -0700

    Initial Commit

---
+++ great number/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  great number
+//
+//  Created by elva wang on 10/31/17.
+//  Copyright © 2017 elva wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
